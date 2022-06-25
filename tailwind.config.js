9c89979250e0e10eb20a0094ce24d1949569734c
moa-mel <52823454+moa-mel@users.noreply.github.com>
2022-06-25 14:26:32 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+  content: [ "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
